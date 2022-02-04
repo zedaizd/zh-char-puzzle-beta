7ae32d2bec994cf64472871250ceb337f7709835
zedaizd <zedaizd@gmail.com>
2022-02-04 12:57:15 +0800

    Simplify intro

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,7 +11,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="遊玩說明" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         請根據謎面猜中謎底的國字<br/>
-        這幾張字卡是謎面範例<br />
+        下面是謎面範例，有三張字卡<br />
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -22,10 +22,10 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
 
       <p className="text-sm text-gray-500 dark:text-gray-300">
         字卡上是拆解國字後得到的部件<br/>
-        遊戲保證所有組成謎底的部件都藏在謎面中<br /><br />
+        所有組成謎底的部件都藏在謎面中<br /><br />
 
-        你有六次機會找出謎底，每次可以猜一字<br />
-        用猜測字的部件打謎面，改變謎面字卡的顏色<br />
+        共有六次機會找出謎底，每次可試一字<br />
+        用字的部件打謎面，改變字卡的顏色<br />
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -34,7 +34,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <HintKey symbol={73} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        例如我猜「友」，打中最左邊的卡<br />
+        例如猜「友」，打中左邊的卡<br />
         卡片變綠色表示謎底有這個部件<br />
       </p>
 
@@ -44,10 +44,10 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <HintKey symbol={73} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        例如我猜「古」，打中中間的卡<br />
+        例如猜「古」，打中中間的卡<br />
         卡片變紅色表示謎底沒有這個部件<br /><br />
 
-        謎題跟可以猜的字出自
+        謎底跟可以猜的字出自
         <a
          href="https://language.moe.gov.tw/result.aspx?classify_sn=23&subclassify_sn=437&content_sn=46"
          className="underline font-bold"
